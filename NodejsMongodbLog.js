c618f317734f2488d7713eefd20f138ee87dcb74
Yang <1847037115@qq.com>
2021-12-08 17:35:40 -0800

    install mongoose successfully

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,17 @@
-const express = require("express")
+const express = require("express")//引进express
 const app=express()
+const mongoose = require('mongoose');//引进mongoose
+mongoose.connect('mongodb://localhost/test1');//连接数据库
+const schema={
+    name:String,
+    age:Number,
+    health:String,
+    hobby:String
+}
+const mydata = mongoose.model('cat1s',schema );
+const kitty = new mydata({ name: 'testZildjian2' });
+kitty.save()
+
 app.use('/', express.static('public'))
 app.get("/input",(req,res)=>{
      res.send(req.query)
