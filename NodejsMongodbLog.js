033d114a06e7d696bebd67ce47e50ae029cc990f
Yang <1847037115@qq.com>
2021-12-16 04:02:34 -0800

    mongodb success web!

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,7 +1,7 @@
 const express = require("express")//引进express
 const app = express()
 const mongoose = require('mongoose');//引进mongoose
-const ejs = require("ejs")
+const ejs = require('ejs')
 mongoose.connect('mongodb://172.21.2.236:27017/190110890317');//连接数据库
 const schema = {
     name: String,
@@ -15,15 +15,16 @@ const mydata = mongoose.model('cat1s', schema);
 
 app.use('/', express.static('public'))
 app.get("/input", (req, res) => {
-    res.send(req.query)
-    console.log(req.query)
+    //res.send(req.query)
+    //console.log(req.query)
     const kitty = new mydata({ name: req.query.first, health: req.query.second });
     kitty.save()
     // ejs.renderFile(filename, data, options, function (err, str) {
     //     //str => 输出渲染后的 HTML 字符串
     // });
-    ejs.renderFile("result.html",{returnVal:"success"},(err,str)=>{
+    returnval = { returnMessage: "success" }
+    ejs.renderFile('result.html', returnval, function (err, str){
         res.send(str)
-    })
+    });
 })
 app.listen(50317)
